71756922609d82e7089cc4182c8140b0af6b1744
Josselin <julien.josselin75@hotmail.fr>
2023-10-18 10:17:55 +0200

    dirty: start writing fetch

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Velib ui")
+console.log("Velib ui");
+
+fetch(
+  "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_information.json",
+  {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "Access-Control-Allow-Origin":
+        "https://velib-metropole-opendata.smovengo.cloud",
+    },
+  }
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (response) {
+    console.log(response);
+  });
